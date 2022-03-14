47e24f6eff135f842de14dde79b373227f558250
Iryna Abah <irin.bon@gmail.com>
2022-03-14 16:28:18 +0000

    added Routes

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,16 +1,17 @@
 import "./Header.css";
-import { Link } from 'react-router-dom';
+import { Link, Route } from 'react-router-dom';
 import logo from "../../images/laurus-trust-logo.png";
 import Navigation from "./Nagivation/Nagivation";
 
-const Header = ({logginIn}) => {
+const Header = ({loggedIn}) => {
   return (
     <header className="header">
       <Link to="/">
         <img className="header-logo" src={logo} alt="Laurus Trust logo"/>
       </Link>
-      <Navigation 
-      loggedIn={logginIn}/>
+      <Navigation>
+        loggedIn={loggedIn}
+      </Navigation> 
     </header>
   )
 };
