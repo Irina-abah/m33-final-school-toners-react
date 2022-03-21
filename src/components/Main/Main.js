9fd3275ed24f5d24faab7c7fd40d56432db2ffc8
Iryna Abah <irin.bon@gmail.com>
2022-03-21 15:56:49 +0000

    added schools

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,30 +1,51 @@
 import "./Main.css";
+import { HashLink as Link } from 'react-router-hash-link';
+import chhs from "../../images/cheadle-hulme-high-school.svg";
+import chsf from "../../images/cheadle-hulme-sixth-form.svg";
+import gb from "../../images/gorsey-bank.svg";
+import chps from "../../images/cheadle-hulme-primary-school.svg";
+import lch from "../../images/laurus-cheadle-hulme.svg";
+import lr from "../../images/laurus-ryecroft.svg";
+import dhs from "../../images/didsbury-high-school.svg";
+// import lch from "../../images/laurus-cheadle-hulme.svg";
+// import lch from "../../images/laurus-cheadle-hulme.svg";
 
 const Main = () => {
   return (
     <main className="main">
       <section className="promo">
-      <div className="promo__container">
-        {/* <img className="promo__image" src={PromoIcon} alt="Main page promo logo" /> */}
-        <h1 className="promo__title">Учебный проект студента факультета Веб-разработки.</h1>
-      </div>
-    </section>
-    <section className="techs" id="techs">
-      <h2 className="section__title">Technologies</h2>
-      <div className="techs__info">
-        <h3 className="techs__title">7 tecnhologies</h3>
-        <p className="techs__subtitle">Technology stack used for this project:</p>
-      </div>
-      <ul className="techs__list">
-        <li className="techs__item">HTML</li>
-        <li className="techs__item">CSS</li>
-        <li className="techs__item">Git</li>
-        <li className="techs__item">React</li>
-        <li className="techs__item">Node.js</li>
-        <li className="techs__item">Express</li>
-        <li className="techs__item">MySQL</li>
-      </ul>
-    </section>
+        <h1 className="promo__title">This app helps to keep Printer toners levels up-to-date.</h1>
+        <p className="promo__subtitle">Login or create an account to manage stock levels for all <Link className="link promo__span" to="#schools">schools of Laurus Trust</Link> </p>
+      </section>
+      <section className="techs">
+        <h2 className="section__title">Technologies</h2>
+        <div className="techs__info">
+          <h3 className="techs__title">7 tecnhologies</h3>
+          <p className="techs__subtitle">Technology stack used for this project:</p>
+        </div>
+        <ul className="techs__list">
+          <li className="techs__item">HTML</li>
+          <li className="techs__item">CSS</li>
+          <li className="techs__item">Git</li>
+          <li className="techs__item">React</li>
+          <li className="techs__item">Node.js</li>
+          <li className="techs__item">Express</li>
+          <li className="techs__item">MySQL</li>
+        </ul>
+      </section>
+      <section className="schools" id="schools">
+      <h2 className="section__title">Laurus Trust Schools</h2>
+        <ul className="schools__list">
+          <li className="school__item"><img className="school__photo" src={chhs} alt="chhs"/></li>
+          <li className="school__item green"><img className="school__photo" src={chsf} alt="chsf"/></li>
+          <li className="school__item"><img className="school__photo" src={gb} alt="gb"/></li>
+          <li className="school__item"><img className="school__photo" src={chps} alt="chps"/></li>
+          <li className="school__item green"><img className="school__photo" src={lch} alt="lch"/></li>
+          <li className="school__item"><img className="school__photo" src={lr} alt="lr"/></li>
+          <li className="school__item"><img className="school__photo" src={dhs} alt="dhs"/></li>
+          <li className="school__item green">Node.js</li>
+        </ul>
+      </section>
     </main>
   )
 }
